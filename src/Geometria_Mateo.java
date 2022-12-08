b5edb53491df44844f12d7dbeb2de1053819250f
Sebastian Congo <96399138+Einarr07@users.noreply.github.com>
2022-12-08 18:46:19 -0500

    Cambios 8/12/22

--- src/Geometria_Mateo.java
+++ src/Geometria_Mateo.java
@@ -4,6 +4,8 @@ public class Geometria_Mateo {
     float base;
     float altura;
     float total;
+    float lados;
+    float lado2;
     Scanner entrada = new Scanner(System.in);
     //Constructor
     public Geometria_Mateo(){
@@ -41,7 +43,7 @@ public class Geometria_Mateo {
     //Menu para escoger las figuras
     int menu_figuras(){
         int op = -1;
-        while ((op < 0) || (op > 4)) {
+        while ((op < 0) || (op > 3)) {
             System.out.println("--------------------------------");
             System.out.println("| ESCOJA UNA FIGURA GEOMETRICA |");
             System.out.println("--------------------------------");
@@ -52,12 +54,11 @@ public class Geometria_Mateo {
             System.out.println("|                                           |");
             System.out.println("|     3.- Rectangulos                       |");
             System.out.println("|                                           |");
-            System.out.println("|     4.- SALIR                             |");
             System.out.println("---------------------------------------------");
             System.out.print("Ingrese la opcion que desea ejecutar: ");
             op = entrada.nextInt();
 
-            if((op < 0) || (op > 4)){
+            if((op < 0) || (op > 3)){
                 System.out.println("--------------------");
                 System.out.println("| Opcion no valida |");
                 System.out.println("--------------------");
@@ -65,15 +66,121 @@ public class Geometria_Mateo {
         }
         return op;
     }//Fin del menu_figuras
+    int menu_triangulos(){
+        int op = -1;
+        while ((op < 5) || (op > 7)) {
+            System.out.println("--------------------------------");
+            System.out.println("| ESCOJA el tipo de triangulo  |");
+            System.out.println("--------------------------------");
+            System.out.println("---------------------------------------------");
+            System.out.println("|     5.- Equilatero                        |");
+            System.out.println("|                                           |");
+            System.out.println("|     6.- Isoseles                          |");
+            System.out.println("|                                           |");
+            System.out.println("|     7.- Escaleno                          |");
+            System.out.println("|                                           |");
+            System.out.println("---------------------------------------------");
+            System.out.print("Ingrese la opcion que desea ejecutar: ");
+            op = entrada.nextInt();
+
+            if((op < 5) || (op > 7)){
+                System.out.println("--------------------");
+                System.out.println("| Opcion no valida |");
+                System.out.println("--------------------");
+            }
+        }
+        return op;
+    }//Fin del menu_triangulos
     //Metodos
-    public void cuadrados(Geometria_Mateo datos1){
+    // Areas de las figuras geometricas
+    public void cuadrado_areas(Geometria_Mateo datos1){
         System.out.println("Ingrese la base del cuadrado");
         datos1.setBase(entrada.nextFloat());
         System.out.println("Ingrese la altura del cuadrado");
         datos1.setAltura(entrada.nextFloat());
+        if (getBase() != getAltura()){
+            System.out.println("Si la base o la altura son diferentes, no es un cuadrado");
+        }
+        else {
+            total = getAltura() * getBase();
+            System.out.println("El area del cuadrado es: " + total);
+        }
+    }
+    public void triangulo_area(Geometria_Mateo datos2){
+        System.out.println("Ingrese la base del triangulo");
+        datos2.setBase(entrada.nextFloat());
+        System.out.println("Ingrese la altura del triangulo");
+        datos2.setAltura(entrada.nextFloat());
+        total = (getBase() * getAltura()) / 2;
+        System.out.println("El area del triangulo es: " + total);
+    }
+    public void rectangulo_area(Geometria_Mateo datos3){
+        System.out.println("Ingrese la base del regtangulo");
+        datos3.setBase(entrada.nextFloat());
+        System.out.println("Ingrese la altura del regtangulo");
+        datos3.setAltura(entrada.nextFloat());
+        total = getBase() * getAltura();
+        System.out.println("El area del regtangulo es: " + total);
 
     }
+    //Perimetro de las figuras geometricas
+    public void cuadrado_perimetro(Geometria_Mateo datos4){
+        System.out.println("Ingrese la dimension del cuadrado");
+        datos4.setLados(entrada.nextFloat());
+        total = getLados() + getLados() + getLados() + getLados();
+        System.out.println("El perimetro del cuadrado es: " +  total);
+    }
+    public void triangulo_perimetro_equi(Geometria_Mateo datos5){
+        System.out.println("Ingrese el lado del triangulo");
+        datos5.setLados(entrada.nextFloat());
+        total = getLados() * 3;
+        System.out.println("El perimetro del triangulo equilatero es: " + total);
+    }
+    public void triangulo_perimetro_iso(Geometria_Mateo datos5){
+        System.out.println("Ingrese el valor de los lados del triangulo");
+        datos5.setLados(entrada.nextFloat());
+        System.out.println("Ingrese la base del triangulo");
+        datos5.setBase(entrada.nextFloat());
+        total = (2 * getLados()) + getBase();
+        System.out.println("El perimetro del triangulo isoseles es: " + total);
+    }
+    public void triangulo_perimetro_esca(Geometria_Mateo datos5){
+        System.out.println("Ingrese el primer lado del triangulo");
+        datos5.setLados(entrada.nextFloat());
+        System.out.println("Ingrese el segundo lado del triangulo");
+        datos5.setLado2(entrada.nextFloat());
+        System.out.println("Ingrese el tercer lado del triangulo");
+        datos5.setBase(entrada.nextFloat());
+        total = getLados() + getLado2() + getBase();
+        System.out.println("El perimetro del triangulo escaleno es: " + total);
+    }
+    public void rectangulo_perimetro(Geometria_Mateo datos6){
+        System.out.println("Ingrese la altura del rectangulo");
+        datos6.setAltura(entrada.nextFloat());
+        System.out.println("Ingrese la base del rectangulo");
+        datos6.setBase(entrada.nextFloat());
+        total = getAltura() + getAltura() + getBase() + getBase();
+        System.out.println("El perimetro del regtangulo es: " +  total);
+    }
+    //Volumen de las figuras geometricas
+
     //SETTER AND GETTER
+
+    public float getLado2() {
+        return lado2;
+    }
+
+    public void setLado2(float lado2) {
+        this.lado2 = lado2;
+    }
+
+    public float getLados() {
+        return lados;
+    }
+
+    public void setLados(float lados) {
+        this.lados = lados;
+    }
 
     public float getBase() {
         return base;
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+    //Instancias
     Geometria_Mateo fig_cuadrado = new Geometria_Mateo();
+    Geometria_Mateo fig_triangulo = new Geometria_Mateo();
+    Geometria_Mateo fig_regtangulo = new Geometria_Mateo();
     //
     Geometria_Mateo menu_de_figuras = new Geometria_Mateo();
     Geometria_Mateo menu_de_opciones = new Geometria_Mateo();
@@ -12,12 +15,36 @@ public class Main {
         switch (menu_de_opciones.menu()){
             case 1:
                 op_figuras = menu_de_figuras.menu_figuras();
-                if (op_figuras >= 1){
-                    fig_cuadrado.cuadrados(fig_cuadrado);
+                if (op_figuras == 1){
+                    fig_cuadrado.cuadrado_areas(fig_cuadrado);
+                }
+                if (op_figuras == 2){
+                    fig_triangulo.triangulo_area(fig_triangulo);
+                }
+                if (op_figuras == 3){
+                    fig_regtangulo.rectangulo_area(fig_regtangulo);
                 }
                 break;
             case 2:
-
+                op_figuras = menu_de_figuras.menu_figuras();
+                if (op_figuras == 1){
+                    fig_cuadrado.cuadrado_perimetro(fig_cuadrado);
+                }
+                if (op_figuras == 2){
+                    op_figuras = menu_de_figuras.menu_triangulos();
+                    if (op_figuras ==5 ){
+                    fig_triangulo.triangulo_perimetro_equi(fig_triangulo);
+                    }
+                    else if (op_figuras ==6) {
+                    fig_triangulo.triangulo_perimetro_iso(fig_triangulo);
+                    }
+                    else if (op_figuras == 7){
+                    fig_triangulo.triangulo_perimetro_esca(fig_triangulo);
+                    }
+                }
+                if (op_figuras == 3){
+                    fig_regtangulo.rectangulo_perimetro(fig_regtangulo);
+                }
                 break;
             case 3:
 
